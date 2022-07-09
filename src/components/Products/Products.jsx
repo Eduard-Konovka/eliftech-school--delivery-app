ba15b7566510349c2012a36e5db0fe19108c164d
Eduard-Konovka <ed098ua@gmail.com>
2022-07-09 20:26:29 +0300

    Adds netlify deploy

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,10 +2,10 @@ import s from './Products.module.css';
 
 export default function Products() {
   return (
-    <p className={s.products}>
+    <div className={s.products}>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatem
       ipsam molestiae rem nihil exercitationem repudiandae, earum autem
       consequuntur sapiente.
-    </p>
+    </div>
   );
 }
