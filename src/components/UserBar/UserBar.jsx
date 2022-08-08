fa02fd08c6162d46d714b5ceaf213416ad077f95
Eduard Konovka <ed098ua@gmail.com>
2022-08-08 19:47:23 +0300

    Fix phone pattern

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -40,7 +40,7 @@ export default function UserBar({ user, setUser }) {
         <input
           type="tel"
           name="phone"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           defaultValue={user.phone}
