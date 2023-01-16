a85b95344e5b912522490221a29278e10157171f
Eduard Konovka <ed098ua@gmail.com>
2023-01-17 00:02:45 +0200

    Adds ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
+import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );
